2b48f647bebd686f8e018ce74939a8b4605a2cf2
Daniel <drgerber69@gmail.com>
2024-06-20 14:00:05 +0200

    Styling the webpage

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,17 @@
 export default function SideBar() {
     return (
-        <div>
-
+        <div className="sidebar">
+            <div className="bgOverlay"></div>
+            <div className="sidebarContents">
+                <h2>The Brutal Martain Landscape</h2>
+                <div>
+                    <p>Description</p>
+                    <p>fgsfdgfsdgfdssgsfdgfdsgfsdgsdff fdg fdgsfdgfdsg</p>
+                </div>
+                <button>
+                    <i className="fa-solid fa-circle-arrow-right"></i>
+                </button>
+            </div>
         </div>
     )
 }
